facc30f9b3e8bd10ef0b36caaf6fc55ee839f569
Jerome Terry <jerome.l.terry@gmail.com>
2013-05-23 14:36:56 -0230

    Playing with MS Code Contracts
    Adding a sample project to play around with Microsoft Code Contracts
    http://research.microsoft.com/en-us/projects/contracts/

---
+++ CodeContracts/CodeContracts/MyStack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+
+namespace CodeContracts
+{
+    public class MyStack<T>
+    {
+        private int capacity;
+        private int count;
+        private T[] representation;
+
+        public MyStack(int capacity)
+        {
+            this.capacity = capacity;
+            representation = new T[capacity];
+        }
+
+        private int Top
+        {
+            get
+            {
+                Contract.Requires(Empty == false);
+                return count - 1;
+            }
+        }
+
+        public bool Empty
+        {
+            get
+            {
+                return count == 0;
+            }
+        }
+
+        public bool Full
+        {
+            get
+            {
+                return count == capacity;
+            }
+        }
+        
+        public void Put(T item)
+        {
+            Contract.Requires(Full == false);
+            Contract.Ensures(Empty == false);
+            representation[count] = item;
+            count += 1;
+        }
+
+        public T Remove()
+        {
+            Contract.Requires(Empty == false);
+            T item = representation[Top];
+            count -= 1;
+            return item;
+        }
+
+        public T Item()
+        {
+            Contract.Requires(Empty == false);
+            return representation[Top];
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(representation != null);
+            Contract.Invariant(count >= 0);
+            Contract.Invariant(capacity >= 0);
+            Contract.Invariant(count <= capacity);
+        }
+    }
+}
---
+++ CodeContracts/CodeContracts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeContracts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new MyStack<int>(10);
+            s.Put(1);
+            s.Put(2);
+            s.Put(3);
+            int top = s.Item();
+        }
+    }
+}
