4838d2170d38ca56f8b6fecee43d88b60fdd4f9f
Jerome Terry <jerome.l.terry@gmail.com>
2013-06-03 14:38:48 -0230

    Adding winforms app that just runs ScriptEditor
    Adding a Win Forms .NET application that shows the ScriptEditor form on
    program startup. This app differs from the jterry.scripting.winforms
    app, which represents an existing application with scripting as an add
    on. jterry.scripting.api.script.app exists solely to execute the api
    using IronPython

---
+++ DotNetScripting/jterry.scripting/jterry.scripting.api.script.app/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using jterry.scripting.host;
+using jterry.scripting.host.editor;
+using jterry.scripting.api;
+
+namespace jterry.scripting.api.script.app
+{
+    static class Program
+    {
+        static ScriptHost _scriptHost;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            _scriptHost = new ScriptHost();
+            var dlg = new ScriptEditor();
+            dlg.Script = LoadDefaultScript();
+            dlg.ScriptHost = _scriptHost;
+            IFactory factory = new Factory();
+            dlg.ScriptHost.RegisterVariable("factory", factory);
+
+            Application.Run(dlg);
+        }
+
+        private static string LoadDefaultScript()
+        {
+            string file = Properties.Settings.Default.DefaultScript;
+            string script = System.IO.File.ReadAllText(file);
+            return script;
+        }
+    }
+}
