be97874ad41ea554487bdf305bc0d19c1e9fa1c7
Jerome Terry <jerome.l.terry@gmail.com>
2013-06-11 14:27:13 -0230

    Updating scripts and cleaning up using statments

--- DotNetScripting/jterry.scripting/jterry.scripting.api/IUnitOfWork.cs
+++ DotNetScripting/jterry.scripting/jterry.scripting.api/IUnitOfWork.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
+﻿using System.Linq;
 
 namespace jterry.scripting.api
 {
--- DotNetScripting/jterry.scripting/jterry.scripting.web/Scripts/DefaultScript.py
+++ DotNetScripting/jterry.scripting/jterry.scripting.web/Scripts/DefaultScript.py
@@ -3,34 +3,87 @@ import System
 clr.AddReference("System.Core")
 clr.AddReference("System.Windows.Forms")
 clr.ImportExtensions(System.Linq)
+from System import String
 
-customers = unitOfWork.GetCustomers()
-employees = unitOfWork.GetEmployees()
-invoices = unitOfWork.GetInvoices()
-playlists = unitOfWork.GetPlaylists()
+def TestIronPython():
+    CountEntities()
+    TestCustomers()
+    TestInvoices()
+    TestPlaylists()
+    TestSearchTracks()
 
-customer = customers.FirstOrDefault()
-rep = customer.SupportRep
-reportsTo = rep.ReportsTo
+def GetAllCustomers():
+    return unitOfWork.GetCustomers()
 
-print "Found " + customers.Count().ToString() + " customers"
-print "Found " + employees.Count().ToString() + " employees"
-print "Found " + invoices.Count().ToString() + " invoices"
+def GetAllEmployees():
+    return unitOfWork.GetEmployees()
 
-print "Customer: " + customer.FirstName + " " + customer.LastName
-print "Support Rep: " + rep.FirstName + " " + rep.LastName
+def GetAllInvoices():
+    return unitOfWork.GetInvoices()
 
-if (reportsTo != None):
-    print "Reports To: " + reportsTo.FirstName + " " + reportsTo.LastName
-else:
-    print "Employee has no boss"
+def GetAllPlaylists():
+    return unitOfWork.GetPlaylists()
 
-invoice = invoices.FirstOrDefault()
-lines = invoice.Lines
+def GetAllTracks():
+    return unitOfWork.GetTracks()
 
-print "Tracks"
-for l in lines:
-    print l.Track.Name
+def CountEntities():
+    WriteLine(String.Format("Found {0} customers", GetAllCustomers().Count()))
+    WriteLine(String.Format("Found {0} employees", GetAllEmployees().Count()))
+    WriteLine(String.Format("Found {0} invoices", GetAllInvoices().Count()))
+    WriteLine()
 
-playlist = playlists.FirstOrDefault()
-print playlist.Name
+def TestCustomers():
+    WriteLine("Testing Customers...")
+    customer = GetAllCustomers().FirstOrDefault()
+    rep = customer.SupportRep
+    reportsTo = rep.ReportsTo
+    
+    WriteLine(String.Format("Customer: {0} {1}", customer.FirstName, customer.LastName))
+    WriteLine(String.Format("Support Rep: {0} {1}", rep.FirstName, rep.LastName))
+
+    if (reportsTo != None):
+        WriteLine(String.Format("Reports To: {0} {1}", reportsTo.FirstName, reportsTo.LastName))
+    else:
+        WriteLine("Employee has no boss")
+
+    WriteLine("Testing Customers complete");
+    WriteLine()
+
+def WriteLine(line = ""):
+    print line
+
+def TestPlaylists():
+    WriteLine("Testing Playlists...")
+    playlist = GetAllPlaylists().FirstOrDefault()
+    WriteLine(playlist.Name)
+    WriteLine("Testing Playlists complete")
+    WriteLine()
+
+def TestInvoices():
+    WriteLine("Testing Invoices...")
+    invoice = GetAllInvoices().FirstOrDefault()
+    WriteLine(String.Format("Invoice Id: {0} Date: {1}", invoice.Id, invoice.InvoiceDate))
+    lines = invoice.Lines
+    WriteLine(String.Format("Found {0} tracks", lines.Count))
+    for l in lines:
+        WriteLine(String.Format("Invoice Line Id: {0} Track Id: {1} Track Name: {2} Price: {3}", 
+                                l.Id, l.Track.Id, l.Track.Name, l.Track.UnitPrice))
+    WriteLine("Testing Invoices complete")
+    WriteLine()
+
+def TestSearchTracks():
+    WriteLine("Testing Search Tracks...");
+    SearchTracks("AC/DC")
+    WriteLine("Testing Search Tracks Complete");
+    WriteLine()
+
+def SearchTracks(composer):
+    WriteLine(String.Format("Searching for tracks by '{0}'...", composer))
+    tracks = GetAllTracks().Where(lambda t: t.Composer == composer)
+    WriteLine(String.Format("Found {0} tracks by '{1}'", tracks.Count(), composer))
+    for t in tracks:
+        WriteLine(String.Format("Track Id: {0} Name: {1}", t.Id, t.Name))
+    WriteLine("Search complete")
+
+TestIronPython()
