4f89e770c10abec10f75b22887a43a0784c9504e
Jerome Terry <jerome.l.terry@gmail.com>
2013-06-11 14:28:05 -0230

    Cleaning up using statements

--- DotNetScripting/jterry.scripting/jterry.scripting.api/ChinookContext.cs
+++ DotNetScripting/jterry.scripting/jterry.scripting.api/ChinookContext.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace jterry.scripting.api
 {
