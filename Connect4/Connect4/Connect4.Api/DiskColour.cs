f0ea7c897188a2edcd5cc983e334d557a67d9492
Jerome Terry <jerome.l.terry@gmail.com>
2013-09-25 19:46:35 -0230

    Adding a Wall class representing the game board
    The game board is a vertically suspended grid, typically 6 rows x 7
    columns, where players insert a single coloured disk at time into a
    single column.

---
+++ Connect4/Connect4/Connect4.Api/DiskColour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Connect4.Api
+{
+    public enum DiskColour
+    {
+        Empty,
+        Red,
+        Black
+    }
+}
