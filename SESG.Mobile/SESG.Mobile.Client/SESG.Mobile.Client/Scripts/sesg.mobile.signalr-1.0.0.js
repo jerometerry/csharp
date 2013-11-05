345a06f7bd22e0d90c9b84dc3261ff66bb075eb4
Jerome Terry <jerome.l.terry@gmail.com>
2013-11-05 14:21:08 -0330

    Pulling out the SignalR JavaScipt

---
+++ SESG.Mobile/SESG.Mobile.Client/SESG.Mobile.Client/Scripts/sesg.mobile.signalr-1.0.0.js
@@ -0,0 +1,29 @@
+﻿function SesgSignalrClient(signalrServerUrl, messageRecievedCallback) {
+
+    var signalrClient = this;
+
+    // Declare a proxy to reference the hub. 
+    this.chat = $.connection.chatHub;
+
+    this.messageRecievedCallback = messageRecievedCallback;
+
+    this.signalrServerUrl = signalrServerUrl;
+    this.ready = false;
+
+    // Create a function that the hub can call to broadcast messages.
+    this.chat.client.broadcastMessage = this.messageRecievedCallback;
+
+    this.send = function (user, message) {
+        this.chat.server.send(user, message);
+    };
+
+    // Start the connection.
+    $.connection.hub.url = this.signalrServerUrl + '/signalr/hubs';
+    $.connection.hub
+        .error(function (data) {
+        }).start({ jsonp: true }).done(function () {
+            signalrClient.ready = true;
+        });
+
+    return this;
+}
