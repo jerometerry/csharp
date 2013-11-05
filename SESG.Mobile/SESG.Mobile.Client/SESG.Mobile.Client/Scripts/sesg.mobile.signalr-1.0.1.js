cb1041b3a9d0c758fb7f9443ae973cf22123c618
Jerome Terry <jerome.l.terry@gmail.com>
2013-11-05 16:09:22 -0330

    Refactoring SignalR Client
    Refactoring the SignalR client so that the connection settings are
    separate from the code. This allows the client to be updated, without
    having to worry about overwriting the connection strings.

---
+++ SESG.Mobile/SESG.Mobile.Client/SESG.Mobile.Client/Scripts/sesg.mobile.signalr-1.0.1.js
@@ -0,0 +1,29 @@
+﻿function SesgSignalrClient(signalrServerUrl, messageRecievedCallback) {
+
+    var signalrClient = this;
+    signalrClient.signalrServerUrl = signalrServerUrl;
+    var signalrHubsUrl = apiUrl + '/signalr/hubs'
+    signalrClient.messageRecievedCallback = messageRecievedCallback;
+    signalrClient.ready = false;
+
+    signalrClient.send = function (user, message) {
+        signalrClient.chat.server.send(user, message);
+    };
+    
+    signalrClient.initialize = function () {
+        // Declare a proxy to reference the hub. 
+        signalrClient.chat = $.connection.chatHub;
+        // Create a function that the hub can call to broadcast messages.
+        signalrClient.chat.client.broadcastMessage = this.messageRecievedCallback;
+
+        // Start the connection.
+        $.connection.hub.url = signalrHubsUrl;
+        $.connection.hub
+            .error(function (data) {
+            }).start({ jsonp: true }).done(function () {
+                signalrClient.ready = true;
+            });
+    }
+
+    return signalrClient;
+}
