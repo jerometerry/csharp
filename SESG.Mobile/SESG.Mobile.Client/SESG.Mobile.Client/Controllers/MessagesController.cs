0e411c1b4bb8e6e54103f4bfba342ff56d831b19
Jerome Terry <jerome.l.terry@gmail.com>
2013-11-04 23:34:19 -0330

    Adding simple SignalR messenger

---
+++ SESG.Mobile/SESG.Mobile.Client/SESG.Mobile.Client/Controllers/MessagesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SESG.Mobile.Client.Controllers
+{
+    public class MessagesController : Controller
+    {
+        //
+        // GET: /Messages/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SESG.Mobile/SESG.Mobile.Service/SESG.Mobile.Service/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SESG.Mobile.Service.Startup))]
+namespace SESG.Mobile.Service
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
