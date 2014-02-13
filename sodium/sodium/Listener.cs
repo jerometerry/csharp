13a049084eb57fae2e93f0b2e0faaa170ddd7e59
Jerome Terry <jerome.l.terry@gmail.com>
2014-02-13 11:47:42 -0330

    Fixing the Listener class

--- sodium/sodium/Listener.cs
+++ sodium/sodium/Listener.cs
@@ -3,20 +3,32 @@ namespace sodium {
     public class Listener {
         public Listener() {}
 
-        public void unlisten() {}
+        public virtual void unlisten() {}
 
         /**
          * Combine listeners into one where a single unlisten() invocation will unlisten
          * both the inputs.
          */
-        public final Listener append(Listener two) {
-            final Listener one = this;
-            return new Listener() {
-                public void unlisten() {
-                    one.unlisten();
-                    two.unlisten();
-                }
-            };
+        public Listener append(Listener two) {
+            return new DualListener(this, two);
+        }
+
+        private class DualListener : Listener
+        {
+            private Listener listener1;
+            private Listener listener2;
+
+            public DualListener(Listener listener1, Listener listener2)
+            {
+                this.listener1 = listener1;
+                this.listener2 = listener2;
+            }
+
+            public override  void unlisten()
+            {
+                listener1.unlisten();
+                listener2.unlisten();
+            }
         }
     }
 
