d7b3a3ba4be8e4347013cd5cdca792d2627f9cd3
Jerome Terry <jerome.l.terry@gmail.com>
2014-02-17 01:06:47 -0330

    Adding a Maybe class

---
+++ sodium/sodium/Maybe.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sodium
+{
+    public class Maybe<T>
+    {
+        private T _value;
+        private bool _hasValue;
+
+        public static Maybe<T> Null
+        {
+            get
+            {
+                return new Maybe<T>(default(T), false);
+            }
+        }
+
+        public Maybe()
+        {
+            _hasValue = false;
+        }
+
+        public Maybe(T value)
+        {
+            _value = value;
+            _hasValue = true;
+        }
+
+        public Maybe(T value, bool hasValue)
+        {
+            _value = value;
+            _hasValue = hasValue;
+        }
+
+        public bool HasValue
+        {
+            get
+            {
+                return _hasValue;
+            }
+        }
+
+        public T Value
+        {
+            get
+            {
+                return _value;
+            }
+        }
+    }
+}
