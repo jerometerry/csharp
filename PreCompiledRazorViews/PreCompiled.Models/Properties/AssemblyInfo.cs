9a3937cc7e388833221eeba925cfd65ad6b9f1f0
Jerome Terry <jerome.l.terry@gmail.com>
2014-08-22 00:33:41 -0230

    Updating assembly names

--- PreCompiledRazorViews/PreCompiled.Models/Properties/AssemblyInfo.cs
+++ PreCompiledRazorViews/PreCompiled.Models/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharedModels")]
+[assembly: AssemblyTitle("PreCompiled.Models")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharedModels")]
+[assembly: AssemblyProduct("PreCompiled.Application")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
